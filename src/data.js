ee8eb4ba63fcbf691ecb1a85158f29248ee44107
pawel-w88 <rws.pawlak@gmail.com>
2023-05-24 13:46:38 +0200

    add data

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const bilder = {
+  fotografie: [
+    {
+      type: "fotografie",
+      author: "Taru Huhkio",
+      title: "Buntstifte",
+      size: "80x80",
+      image: "https://unsplash.com/de/fotos/JQ3JSM--qIg",
+    },
+    {
+      type: "fotografie",
+      author: "Dusan Kipic",
+      title: "Skizzen",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/pyMbXQhPK0k",
+    },
+    {
+      type: "fotografie",
+      author: "Samuel Castro",
+      title: "Black&White",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/BylgpBT4Zvs",
+    },
+    {
+      type: "fotografie",
+      author: "Elena Mozhvilo",
+      title: "Schmetterling",
+      size: "80x80",
+      image: "https://unsplash.com/de/fotos/xw8DfYBaNbQ",
+    },
+  ],
+  kunst: [
+    {
+      type: "kunst",
+      author: "Jene Stephaniuk",
+      title: "Abstrakte",
+      size: "120x80",
+      image: "https://unsplash.com/de/fotos/-MCrF6hnojU",
+    },
+    {
+      type: "kunst",
+      author: "Steve Johnson",
+      title: "Acrylfarbe",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/5MTf9XyVVgM",
+    },
+    {
+      type: "kunst",
+      author: "Gabriela Gomez",
+      title: "Wand Malarei",
+      size: "120x80",
+      image: "https://unsplash.com/de/fotos/3NXyodgX254",
+    },
+    {
+      type: "kunst",
+      author: "Felix Robert",
+      title: "Bunte Fenster",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/CuEvrPd3NYc",
+    },
+  ],
+  graffiti: [
+    {
+      type: "graffiti",
+      author: "Alp Ancel",
+      title: "Aliens",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/WKQt_X-SKFI",
+    },
+    {
+      type: "graffiti",
+      author: "Ben Elwood",
+      title: "Sprays",
+      size: "80x80",
+      image: "https://unsplash.com/de/fotos/rsCB-A69lUc",
+    },
+    {
+      type: "graffiti",
+      author: "John Rodenn",
+      title: "Time",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/g0-N3ftLetM",
+    },
+    {
+      type: "graffiti",
+      author: "Paolo Bendandi",
+      title: "Elefant",
+      size: "100x80",
+      image: "https://unsplash.com/de/fotos/hkpWEhN0os8",
+    },
+  ],
+};
+const bilderData = JSON.stringify(bilder);
+export { bilderData };
---
+++ src/index.js
@@ -0,0 +1,1 @@
+import { bilderData } from "./data.js";
